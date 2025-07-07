3d1e51d54d1564b330b230200525b52baa0eacf8
Md Roknujjaman Rony <roknujjamanrony1234@gmail.com>
2025-07-07 15:09:17 +0600

    added sign in  page

--- src/pages/authpage/Signup.jsx
+++ src/pages/authpage/Signup.jsx
@@ -1,7 +1,19 @@
+import authLogo from "../../assets/others/authentication2.png";
 const Signup = () => {
   return (
     <div>
-      <h3>Signup</h3>
+      {/* gif */}
+      <div>
+        <img src={authLogo} alt="" />
+      </div>
+      {/* input field */}
+      <div>
+        <h1 className="text-black text-2xl font-bold">Login</h1>
+        <form>
+          <label for="email">Email</label>
+          <input type="text" name="email" id="" />
+        </form>
+      </div>
     </div>
   );
 };
