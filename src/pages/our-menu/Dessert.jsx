10bab75b67317dee8c1a3b4b62bd568581fdfdd0
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-07-21 22:05:03 +0600

    added a new page

--- src/pages/our-menu/Dessert.jsx
+++ src/pages/our-menu/Dessert.jsx
@@ -2,18 +2,19 @@ import Cover from "../shared/Cover";
 import coverBg2 from "../../assets/menu/dessert-bg.jpeg";
 
 const Dessert = () => {
+  const [desserts,setDesserts]
   return (
     <div>
       <Cover
         coverBg={coverBg2}
-        heading={"/Desserts"}
-        subHeading={"dessert"}
+        heading={"Desserts"}
+        subHeading={
+          "Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        }
       ></Cover>
-      <Cover
-        coverBg={coverBg2}
-        heading={"/Desserts"}
-        subHeading={"dessert"}
-      ></Cover>
+      <div>
+
+      </div>
     </div>
   );
 };
