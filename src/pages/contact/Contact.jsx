b40b7f6af29fe1b29d359e3dfe626d75ee0204d2
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-07-26 13:12:11 +0600

    modified contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import Cover from "../shared/Cover";
 import contactCover from "../../assets/contact/banner.jpg";
 import SectionTitle from "../../components/shared/SectionTitle";
+import { FaClock, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -15,22 +16,39 @@ const Contact = () => {
           heading={"our-location"}
           subheading={"visit us"}
         ></SectionTitle>
-        <div>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <p></p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 max-w-6xl mx-auto mt-10">
+          {/* Phone Card */}
+          <div className="bg-white shadow-lg">
+            <div className="bg-yellow-600 text-white flex justify-center py-4">
+              <FaPhoneAlt className="text-2xl" />
+            </div>
+            <div className="bg-gray-100 text-center py-6 px-4">
+              <h3 className="text-lg font-semibold">PHONE</h3>
+              <p className="mt-2 text-gray-700">+38 (012) 34 56 789</p>
+            </div>
           </div>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <p></p>
+
+          {/* Address Card */}
+          <div className="bg-white shadow-lg">
+            <div className="bg-yellow-600 text-white flex justify-center py-4">
+              <FaMapMarkerAlt className="text-2xl" />
+            </div>
+            <div className="bg-gray-100 text-center py-6 px-4">
+              <h3 className="text-lg font-semibold">ADDRESS</h3>
+              <p className="mt-2 text-gray-700">+38 (012) 34 56 789</p>
+            </div>
           </div>
-          <div>
-            {" "}
-            <img src="" alt="" />
-            <h2></h2>
-            <p></p>
+
+          {/* Working Hours Card */}
+          <div className="bg-white shadow-lg">
+            <div className="bg-yellow-600 text-white flex justify-center py-4">
+              <FaClock className="text-2xl" />
+            </div>
+            <div className="bg-gray-100 text-center py-6 px-4">
+              <h3 className="text-lg font-semibold">WORKING HOURS</h3>
+              <p className="mt-2 text-gray-700">Mon – Fri: 08:00 – 22:00</p>
+              <p className="text-gray-700">Sat – Sun: 10:00 – 23:00</p>
+            </div>
           </div>
         </div>
       </div>
