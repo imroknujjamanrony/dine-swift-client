01ae63563f74ea5b65cafdf2dfc9255c6b827fa5
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-08-01 22:29:48 +0600

    completed the footer

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -21,6 +21,10 @@ const Footer = () => {
       </aside>
       <nav>
         <h6 className="footer-title">Social</h6>
+        <h6 className="footer-title">Social</h6>
+        <h6 className="footer-title">Social</h6>
+        <h6 className="footer-title">Social</h6>
+        <h6 className="footer-title">Social</h6>
         <div className="grid grid-flow-col gap-4">
           <a>
             <svg
